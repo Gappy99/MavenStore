5e630d563e2bda855b30304ca475ff6fe1f6b8da
kevin ramirez <kramirez-2024399@kinal.edu.gt>
2025-07-13 16:14:39 -0600

    agregando vistas xd

--- src/main/java/org/kevinramirez/controller/InicioVistaController.java
+++ src/main/java/org/kevinramirez/controller/InicioVistaController.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package org.kevinramirez.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author razep
- */
 public class InicioVistaController implements Initializable {
 
     /**
--- src/main/java/org/kevinramirez/system/Main.java
+++ src/main/java/org/kevinramirez/system/Main.java
@@ -1,9 +1,30 @@
 package org.kevinramirez.system;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage escenario) {
+        try {
+            // Cargar el FXML desde resources
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/kevinramirez/view/InicioVista.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            escenario.setTitle("Tiendita Elegante");
+            escenario.setScene(scene);
+            escenario.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
 }
